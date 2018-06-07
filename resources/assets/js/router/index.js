9ab624aa05fa90c221646eb1f21046bd8a9a9f58
Tkach Aleksandr <o.tkach@linet.sumdu.edu.ua>
2018-06-07 20:13:40 +0300

    feature(Home.vue) start

--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -1,9 +1,22 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import NotFound from '../components/NotFound.vue';
+import Home from '../components/Home.vue';
+
 Vue.use(VueRouter);
 
 const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+        meta: {
+            guest: true,
+        },
+    },
+
+    // Must be the last entry in array.
     {
         path: '*',
         component: NotFound,
