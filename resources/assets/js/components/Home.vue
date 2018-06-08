aad3171d34eb28ebcaae2c340fa8f2dab8dc4d25
Tkach Aleksandr <o.tkach@linet.sumdu.edu.ua>
2018-06-08 09:09:04 +0300

    feature(Home.vue) add translations

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -4,43 +4,43 @@
             <div class="col-md-8 col-md-offset-2">
 
                 <div class="panel panel-default">
-                    <div class="panel-heading"> Пользователи </div>
+                    <div class="panel-heading"> {{ $t('translation.addNewUserTitle') }} </div>
 
                     <div class="panel-body">
                         <div class="form-group">
-                            <label for="surname"> Фамилия </label>
+                            <label for="surname"> {{ $t('translation.surname') }} </label>
                             <input type="text" class="form-control" id="surname"
                                    v-model="surname">
                         </div>
                         <div class="form-group">
-                            <label for="name"> Имя </label>
+                            <label for="name"> {{ $t('translation.name') }} </label>
                             <input type="text" class="form-control" id="name"
                                    v-model="name">
                         </div>
                         <div class="form-group">
-                            <label for="middle_name"> Отчество </label>
+                            <label for="middle_name"> {{ $t('translation.middleName') }} </label>
                             <input type="text" class="form-control" id="middle_name"
                                    v-model="middleName">
                         </div>
                         <div class="form-group">
-                            <label for="birthday"> Год рождения </label>
+                            <label for="birthday"> {{ $t('translation.birthday') }} </label>
                             <input type="text" class="form-control" id="birthday"
                                    v-model="birthday">
                         </div>
                         <div class="form-group">
-                            <label for="position"> Должность </label>
+                            <label for="position"> {{ $t('translation.position') }} </label>
                             <input type="text" class="form-control" id="position"
                                    v-model="position">
                         </div>
                         <div class="form-group">
-                            <label for="salary"> Зарплата </label>
+                            <label for="salary"> {{ $t('translation.salary') }}</label>
                             <input type="text" class="form-control" id="salary"
                                    v-model="salary">
                         </div>
 
                         <button type="button" class="btn btn-sm btn-primary pull-right"
                                 @click="addNewUser">
-                            Добавить
+                            {{ $t('translation.addNewUser') }}
                         </button>
                     </div>
                 </div>
